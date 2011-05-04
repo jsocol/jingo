4d6c8d1ebf472ce403be5e369d6a18b2a7894f12
James Socol <james@mozilla.com>
2011-05-03 23:22:58 -0400

    Add a mixin for clas-based views.

--- jingo/views.py
+++ jingo/views.py
@@ -1,5 +1,11 @@
+from django.views.generic.base import TemplateResponseMixin
 import jingo
 
 
 def direct_to_template(request, template, **kwargs):
     return jingo.render(request, template, kwargs)
+
+
+class JingoResponseMixin(TemplateResponseMixin):
+    template_name = None
+    response_class = jingo.JingoTemplateResponse
